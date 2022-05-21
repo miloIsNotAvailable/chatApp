06184b2c155f57b14b21c47af2f79ee90b186086
unknown <milosz20031207@gmail.com>
2022-05-22 00:03:08 +0200

    user search displays users now

--- Components/MainChat/Navbar/Search/Search.tsx
+++ Components/MainChat/Navbar/Search/Search.tsx
@@ -1,36 +1,51 @@
-import { ChangeEvent, ChangeEventHandler, FC, useCallback, useEffect, useMemo } from "react";
+import { ChangeEvent, ChangeEventHandler, FC, useCallback, useEffect, useMemo, useState } from "react";
 import { styles } from "../Build/NavbarStyles";
 import SearchIcon from '../../../../graphics/search.svg'
 import Image from "next/image";
+import { debounce } from "./debounce";
+import { User } from "@prisma/client";
+import { AnimatePresence, motion } from "framer-motion";
 
-const debounce = (fn: any, delay: number) => {
-    let timerId: any;
-    return (...args: any) => {
-      clearTimeout(timerId);
-      timerId = setTimeout(() => fn(...args), delay);
-    }
-  };
 const Search: FC = () => {
 
-    const handleChange = ( e: ChangeEvent<HTMLInputElement> ) => {
+    const [ foundUser, setFoundUser ] = useState<User | null>( null )
 
+    const handleChange = ( 
+        e: ChangeEvent<HTMLInputElement> 
+    ) => {
         fetch( '/api/find_user', {
             method: "POST",
             body: JSON.stringify( {"name": e.target.value} )
-        } ).then( v => v.json() ).then( v => console.log( v ) )
+        } ).then( v => v.json() ).then( v => setFoundUser( v ) )
     }
     
+    // @ts-nocheck
     const e = useCallback( debounce( handleChange, 200 ), [] )
 
     return (
-        <div className={ styles.search_display }>
-            <Image src={ SearchIcon }
-            className={ styles.search_icon }
-            alt=""/>
-            <input 
-                className={ styles.search }
-                placeholder={ 'search' } 
-                onChange={ ( d: any ) =>  e( d ) }/>
+        <div className={ styles.search_wrap }>
+            <div className={ styles.search_display }>
+                <Image src={ SearchIcon }
+                className={ styles.search_icon }
+                alt=""/>
+                <input 
+                    className={ styles.search }
+                    placeholder={ 'search' } 
+                    onChange={ ( d: any ) =>  e( d ) }/>
+            </div>
+            <AnimatePresence>
+                {
+                    foundUser && 
+                    <motion.div 
+                    className={ styles.show_user }
+                    initial={ { height: 0 } }
+                    animate={ { height: '5vw' } }
+                    exit={ { height: 0, opacity: 0 } }>
+                        <div className={ styles.user_profile }/>
+                        { foundUser?.name }
+                    </motion.div>
+                }
+            </AnimatePresence>
         </div>
     )
 }
