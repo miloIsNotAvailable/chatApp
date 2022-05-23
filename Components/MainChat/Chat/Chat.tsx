035e3329b23333c4b33f2fb558d5d2cfeb65c544
unknown <milosz20031207@gmail.com>
2022-05-23 21:05:18 +0200

    added basic websockets

--- Components/MainChat/Chat/Chat.tsx
+++ Components/MainChat/Chat/Chat.tsx
@@ -1,8 +1,19 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import ChatInput from "./ChatInput";
 import { styles } from "./ChatStyles";
+import io from 'socket.io-client'
 
 const Chat: FC = () => {
+
+    const initializeSocket = async() => {
+
+        fetch( '/api/sockets/messages' )
+        const IO = io()
+
+        IO.on( 'connect', () => console.log( 'socket connected' ) )
+    }
+
+    useEffect(  () => {initializeSocket()}, []  )
 
     return (
         <div className={ styles.chat_wrap }>
--- Components/MainChat/Chat/ChatInput.tsx
+++ Components/MainChat/Chat/ChatInput.tsx
@@ -1,23 +1,37 @@
 import Image from "next/image";
-import { FC } from "react";
+import { FC, FormEvent, MouseEvent, useEffect, useRef } from "react";
 import { styles } from "./ChatStyles";
 import UploadImage from "./uploadImage";
 import SendIcon from '../../../graphics/send.svg'
+import io from 'socket.io-client'
 
 const ChatInput: FC = () => {
 
+    const inputRef = useRef<HTMLInputElement | null>( null )
+    const socket = io()
+
+    const handleSubmit = ( 
+        e: FormEvent<HTMLFormElement> | MouseEvent
+        ) => {
+        if( !inputRef.current?.value?.trim() ) return
+        e.preventDefault()
+        
+        socket.emit( 'message', inputRef.current?.value?.trim() )
+    }
+
     return (
-        <div className={ styles.chat_input_wrap }>
+        <form  onSubmit={ handleSubmit } className={ styles.chat_input_wrap }>
             <UploadImage/>
             <input 
+            ref={ inputRef }
             className={ styles.chat_input }
             placeholder={ "send a message" }/>
-            <div className={ styles.send_icon }>
+            <div  onClick={ handleSubmit } className={ styles.send_icon }>
                 <Image
                 src={ SendIcon }
                 alt=""/>
             </div>
-        </div>
+        </form>
     )
 }
 
