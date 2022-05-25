321ff3796635f78ec58e0d00be57fc7e9e60b455
unknown <milosz20031207@gmail.com>
2022-05-25 18:01:36 +0200

    messages now get displayed correctly

--- Components/MainChat/Chat/Chat.tsx
+++ Components/MainChat/Chat/Chat.tsx
@@ -1,44 +1,22 @@
 import { FC, useCallback, useEffect, useState } from "react";
 import ChatInput from "./ChatInput";
 import { styles } from "./ChatStyles";
-import io from 'socket.io-client'
 import { MessageType } from "../../store/interfaces";
 import { initializeSocket } from "./initializeSocket";
-import { useAppSelector } from "../../store/hooks";
-import { map, mergeMap, Observable } from "rxjs";
-import { fromEvent } from 'rxjs'
 import { _io } from "../../constants/WebSocketsConstants";
 import { IOObservable, SocketType } from "../../interfaces/WebSocketsTypes";
-
-type newMessageState = {
-    createMessage: MessageType
-}
+import { listenToMessages } from "./listenToMessages";
 
 const Chat: FC = () => {
 
-    const selector = useAppSelector( ( { createMessage }: newMessageState ) => createMessage )
-    const [ message, setMessage ] = useState<Observable<MessageType> | null>( null )
     const [ msg, setMsg ] = useState<MessageType[] | []>( [] )
-    const socket = io()
 
-    const getVal = ( v: any ) => console.log( v )
-    const e = useCallback( getVal, [] )
+    const handle = ( v: IOObservable<SocketType> ) => 
+    setMsg( ( prev: any[] ): MessageType[] => [ ...prev, v ] )
 
-    useEffect( () => {
-        // socket.on( "msg", e )
-        const m: Observable<IOObservable<SocketType>> = _io.pipe( 
-            mergeMap( ( client ) => 
-              fromEvent( client, 'msg' ).pipe(
-                map(
-                  ( data ) => data
-                )
-              )
-             ) 
-          )
-      
-        m.subscribe( console.log )
-    } )
+    const memoizeReceived = useCallback( () => listenToMessages( handle ), [] )
 
+    useEffect( () => memoizeReceived, [ memoizeReceived ] )
     useEffect(  () => {initializeSocket()}, []  )
     
     return (
---
+++ Components/MainChat/Chat/listenToMessages.ts
@@ -0,0 +1,18 @@
+import { fromEvent, map, mergeMap, Observable } from "rxjs"
+import { _io } from "../../constants/WebSocketsConstants"
+import { IOObservable, SocketType } from "../../interfaces/WebSocketsTypes"
+
+export const listenToMessages: 
+( handle: (v: IOObservable<SocketType>) => void ) => void
+= ( handle ): void => {
+    const m: Observable<IOObservable<SocketType>> = _io.pipe( 
+        mergeMap( ( client ) => 
+          fromEvent( client, 'msg' ).pipe(
+            map(
+              ( data ) => data
+            )
+          )
+         ) 
+      )
+      m.subscribe( handle )
+}
