72112e9af4fc982b3e4765367c915853ee4c1625
unknown <milosz20031207@gmail.com>
2022-05-26 23:51:57 +0200

    added autosizing to textarea when typing

--- Components/MainChat/Chat/ChatInput.tsx
+++ Components/MainChat/Chat/ChatInput.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { FC, FormEvent, KeyboardEvent, MouseEvent, useContext, useEffect, useRef, useState } from "react";
+import { FC, FormEvent, KeyboardEvent, MouseEvent, useContext, useEffect, useLayoutEffect, useRef, useState } from "react";
 import { styles } from "./ChatStyles";
 import UploadImage from "./uploadImage";
 import SendIcon from '../../../graphics/send.svg'
@@ -64,19 +64,32 @@ const ChatInput: FC = () => {
 
         dispatch( newMessage( { msg: inputRef.current?.value?.trim(), room: '' } ) )
         if( inputRef.current ) inputRef.current.value = ''
+        inputRef.current.style.height = 'auto'
     }
     // const handleSubmit = ( 
     //     e: FormEvent<HTMLFormElement> | MouseEvent<any>
     //     ) => Submit( e, inputRef, dispatch, socket, newMessage )
 
+    const changeHeight = () => {
+        if( !inputRef.current ) return
+        
+        let off = inputRef.current?.scrollHeight
+        console.log( off, inputRef.current?.scrollHeight )
+
+        inputRef.current.style.height = off + 'px'
+        if( inputRef.current.value.length === 0 ) inputRef.current.style.height = 'auto'
+    }
+
     return (
         <div className={ styles.chat_input_wrap }>
             <UploadImage/>
             <textarea
+            rows={ 1 }
             ref={ inputRef }
             className={ styles.chat_input }
             placeholder={ "send a message" } 
-            onKeyDown={ handleSubmit }/>
+            onKeyDown={ handleSubmit }
+            onChange={ changeHeight }/>
             <div onClick={ e => evIsMouse( e ) && handleSubmit( e ) } className={ styles.send_icon }>
                 <Image
                 src={ SendIcon }
