d05235ccf023fb6802db0ef892a61054da1e8fae
unknown <milosz20031207@gmail.com>
2022-06-11 21:01:47 +0200

    getting sockets to work in production

--- Components/constants/WebSocketsConstants.ts
+++ Components/constants/WebSocketsConstants.ts
@@ -2,7 +2,10 @@ import { fromEvent, map, of, switchMap } from "rxjs"
 import { io } from "socket.io-client"
 import { IOObservable, SocketType } from "../interfaces/WebSocketsTypes"
 
-export const IO: SocketType = io()
+export const IO: SocketType = io( {
+    reconnection: true,
+    rejectUnauthorized: false
+} )
 
 export const _io: IOObservable<SocketType> = of( IO )
 export const connect = _io.pipe( 
