878659575c31ab01f0b31d6f3d57cb3257fdf943
unknown <milosz20031207@gmail.com>
2022-06-12 18:49:19 +0200

    fixing new channels error in production

--- Components/MainChat/FriendsList/FriendList.tsx
+++ Components/MainChat/FriendsList/FriendList.tsx
@@ -68,13 +68,13 @@ const FriendList: FC = () => {
     if( channels.length === 0 && !selector?.users ) return <ChannelsNotFound/>
 
     if( selector?.users ) return (
-        <FriendListContext value={ { selected, setSelected } }>
+        // <FriendListContext value={ { selected, setSelected } }>
             <>
                 <DisplayNewChannels
                     channels={ channels }
                 />
             </>
-        </FriendListContext>
+        // </FriendListContext>
     ) 
 
     return (
