56de310db3db7e1fb7442d3e6272d36cb53e6371
unknown <milosz20031207@gmail.com>
2022-06-13 21:58:37 +0200

    added error handling

--- Components/MainChat/Chat/chatInput/ChatInput.tsx
+++ Components/MainChat/Chat/chatInput/ChatInput.tsx
@@ -6,11 +6,32 @@ import SendIcon from '../../../../graphics/send.svg'
 import { _io } from "../../../constants/WebSocketsConstants";
 import { useSubmit } from "./handleSubmit/handleSubmit";
 import MainChatTextarea from "./chatTextarea";
+import { useAppSelector } from "../../../store/hooks";
+import { getChannelUsernameState } from "../../../interfaces/mainchatInterfaces";
 
 const ChatInput: FC = () => {
 
     const inputRef = useRef<HTMLDivElement | null>( null )
     const submit = useSubmit<MutableRefObject<HTMLDivElement | null>>( inputRef )
+
+    const selector = useAppSelector( 
+        ( 
+            { channelUsername }: getChannelUsernameState 
+        ) => channelUsername?.name 
+    )
+
+    if( !selector ) return (
+        <div className={ styles.chat_input_wrap }>
+            <UploadImage/>
+            <div></div>
+            {/* <MainChatTextarea inputRef={ inputRef } /> */}
+            <div onClick={ () => {} } className={ styles.send_icon }>
+                <Image
+                src={ SendIcon }
+                alt=""/>
+            </div>
+        </div>
+    )
 
     return (
         <div className={ styles.chat_input_wrap }>
